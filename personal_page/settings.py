c2ab5c0d51889a5633d8a5a327af192db19b4d6d
Rahul Sharma <rahul8448@gmail.com>
2015-02-15 21:13:44 -0600

    Added static files path

--- personal_page/settings.py
+++ personal_page/settings.py
@@ -83,18 +83,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR)
 
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR)
 
-STATICFILES_DIRS=(
-
-    os.path.join(BASE_DIR,'personal_page/static'),
-)
+#STATIC_URL = '/static/'
 
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'personal_page/templates'),
-)
+#STATICFILES_DIRS=(
+
+    #os.path.join(BASE_DIR,'static'),
+#)
+
+
+#TEMPLATE_DIRS = (
+    #os.path.join(BASE_DIR, 'templates'),
+#)
 
 
--- personal_site/settings.py
+++ personal_site/settings.py
@@ -81,9 +81,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR,'static_assets')
+
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
 
+    (os.path.join(BASE_DIR,'personal_page/static')),
+)
 
 LOGGING = {
     'version': 1,

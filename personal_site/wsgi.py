600c04a380dcc369b45ad02956a1c5b6d935aeb9
Rahul Sharma <rahul8448@gmail.com>
2015-02-16 22:11:33 -0600

    changes 3

--- personal_site/wsgi.py
+++ personal_site/wsgi.py
@@ -7,7 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os as os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_site.prod_settings")
 
 from django.core.wsgi import get_wsgi_application

01e0d6bc38e331e02b4850b5fc08c3e59710dc5e
Rahul Sharma <rahul8448@gmail.com>
2015-02-17 21:56:54 -0600

    Adding changes to setting files

--- personal_site/prod_settings.py
+++ personal_site/prod_settings.py
@@ -78,11 +78,3 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-
-
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'personal_page/templates'),
-)
